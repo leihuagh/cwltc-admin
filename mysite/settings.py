1f18881ffa1024fcdc961f6f4d949e50304b75f1
ian <is@ktconsultants.co.uk>
2015-04-08 11:51:55 +0100

    static with /wsgi/

--- mysite/settings.py
+++ mysite/settings.py
@@ -122,7 +122,7 @@ STATICFILES_FINDERS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(BASE_DIR,"static"),
+#   os.path.join(BASE_DIR,"static"),
 #)
 
 TEMPLATE_DIRS = (
