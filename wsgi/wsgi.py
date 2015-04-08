45d08521f48eb870efe6d93feedabf941770f94e
ian <is@ktconsultants.co.uk>
2015-04-08 11:03:48 +0100

    added syst.path to wsgi

--- wsgi/wsgi.py
+++ wsgi/wsgi.py
@@ -8,6 +8,11 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+sys.path.append(os.path.join(os.environ['OPENSHIFT_REPO_DIR']))
+
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
 from django.core.wsgi import get_wsgi_application
