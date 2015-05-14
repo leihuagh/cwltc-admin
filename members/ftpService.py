0fa9caba77c7966d824b6c642b7f8bb5591bab38
ian <is@ktconsultants.co.uk>
2015-05-14 12:34:01 +0100

    parents list

--- members/ftpService.py
+++ members/ftpService.py
@@ -30,3 +30,26 @@ def ftpGet():
     tFile.close
     ftp.quit()
     return 'OK: File size: "%i"' % path.getsize(tPath)
+
+#def ftpPut():
+#    try:
+#        ftp = ftplib.FTP(HOST)
+#    except (socket.error, socket.gaierror), e:
+#        return 'ERROR: cannot reach "%s"' % HOST 
+    
+#    try:
+#        ftp.login(USER,PASS)
+#    except ftplib.error_perm:
+#        ftp.quit()
+#        return 'ERROR: cannot login'
+
+#    try:
+#        ftp.sendcmd("TYPE I")
+#        tPath = path.join(settings.PROJECT_ROOT, FILE)
+#        tFile = open(tPath, 'wb')
+#        ftp.retrbinary('RETR %s' % FILE, tFile.write)
+#    except ftplib.error_perm:
+#        unlink(FILE)
+#        return 'ERROR: cannot read file "%s"' % FILE
+    
+#    ftp.storbinary('STOR %s' % i, open(os.path.join('V://GIS//Maps//County//11x17shd//',i), "rb"))
