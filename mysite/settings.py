bc4de2b44c3e885dd51310f55675278857a36fa2
ian <is@ktconsultants.co.uk>
2016-09-07 08:27:54 +0100

    static root

--- mysite/settings.py
+++ mysite/settings.py
@@ -160,7 +160,7 @@ NUMBER_GROUPING = 3
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'wsgi','static')
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
