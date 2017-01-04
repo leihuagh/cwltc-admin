fc428fa965db925ace25e5ec2006860b76b6d3c4
ian <is@ktconsultants.co.uk>
2017-01-04 09:54:22 +0000

    Updated to Django 1.10.4 also DRF update

--- mysite/settings.py
+++ mysite/settings.py
@@ -181,6 +181,7 @@ TEMPLATES = [
         ],
         'APP_DIRS': True,
         'OPTIONS': {
+            'debug': DEBUG,
             'context_processors': [
                 # Insert your TEMPLATE_CONTEXT_PROCESSORS here or use this
                 # list if you haven't customized them:
