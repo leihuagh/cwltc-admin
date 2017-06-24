db5d02a81dc5c4c871fea4e5c95dacd1c30a82f9
ian <is@ktconsultants.co.uk>
2017-06-24 19:54:35 +0100

    Add js files to members/static

---
+++ members/static/members/scripts/juniors.js
@@ -0,0 +1,91 @@
+﻿// Requires ajaxUrl to be set
+$(document).ready(function () {
+    console.log(ajaxUrl)
+    $('#people').dataTable({
+        "ajax": {
+            "url": ajaxUrl,
+            "type": "POST",
+            "data": function (d) {
+                d.categories = $('#id_categories').val();
+                d.membership_year = $('#id_membership_year').val();
+                d.paystate = $('#id_paystate').val();
+            }
+        },
+        "columns": [null, null, null, null,
+            {
+                "render": function (data, type, row, meta) {
+                    if (type === 'display') {
+                        return $('<a>')
+                            .attr('href', '/' + data)
+                            .text(data)
+                            .wrap('<div></div>')
+                            .parent()
+                            .html();
+                    } else {
+                        return data;
+                    }
+                }
+            }
+        ]
+    });
+});
+
+$('.trigger').change(function () {
+    console.log("Change!")  // sanity check
+    $('#people').DataTable().ajax.reload();
+});
+
+
+// using jQuery
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+// from django documentation
+function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+$.ajaxSetup({
+    beforeSend: function (xhr, settings) {
+        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+            xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+        }
+    }
+})
+
+// AJAX for posting
+function ajax_post() {
+    console.log("AJAX post"); // sanity check
+    $.post(".", {
+        membership_year: $('#id_membership_year').val(),
+        categories: $('#id_categories').val(),
+        paid: $('#id_paid').checked,
+        unpaid: $('#id_unpaid').checked
+    }
+    ).done(function (json) {
+
+        var data = json["data"];
+        //var search = json["search"];
+        $("#people").dataTable(json);
+        //$("#people").DataTable().search(search).draw();
+    }
+    ).fail(function (xhr, errmsg, err) {
+        $('#results').html("<div class='alert-box alert radius' data-alert>Oops! We have encountered an error: " + errmsg +
+            " <a href='#' class='close'>&times;</a></div>"); // add the error to the dom
+        console.log(xhr.status + ": " + xhr.responseText); // provide a bit more info about the error to the console
+    }
+    );
+}
---
+++ members/static/members/scripts/table_checkboxes.js
@@ -0,0 +1,39 @@
+﻿// Handle checkboxes in tables to select rows
+$(document).ready(function () {
+    countChecked();
+   // $('#id_search').onclick = doSearch;
+    document.getElementById('id_search').onclick = doSearch;
+    document.getElementById('id_loader').style.display = 'none';
+    document.getElementById('id_action_form').style.visibility = 'visible';
+    //$('#id_table_panel').show();
+});
+
+
+// toggle / untoggle all checkboxes in the list
+function toggle(source) {
+    var checkboxes = document.getElementsByName('selection');
+    var count=0
+    for (i = 0; i < checkboxes.length; i++)
+        checkboxes[i].checked = source.checked;
+    countChecked();
+}
+
+// Count the number of checked rows
+function countChecked() {
+    var count = document.querySelectorAll('.rowcheckbox:checked').length;
+    
+    var goDisabled = ((count === 0) | (count > 1000));
+    if (count > 1000) {
+        count = "Too many"
+    };
+    document.getElementById('id-count').innerText = count
+    $('#id_action').prop('disabled', goDisabled);
+    $('#id_go').prop('disabled', goDisabled);
+}
+
+function doSearch() {
+    $('#id_loader').show();
+    document.getElementById('id_action_form').style.visibility = 'hidden';
+    document.getElementById('id_loader').style.display = 'inherit';
+    document.getElementById('id_search_form').submit();
+}
