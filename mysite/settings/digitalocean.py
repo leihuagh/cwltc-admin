ab1b374d0aa7db383d6fa51dc3d09bf1b0de2ef0
ianastewart <is@iskt.co.uk>
2017-09-11 07:24:16 +0100

    Fixed digitalocean settings and wsgi

--- mysite/settings/digitalocean.py
+++ mysite/settings/digitalocean.py
@@ -1,19 +1,22 @@
 import socket
 from .base import *
-DEBUG = False
+DEBUG = True
 # We could use the default path but make it explicit for clarity
 env_path = os.path.join(BASE_DIR, "mysite", "settings", ".env")
 environ.Env.read_env(env_path)
 
 DATABASES = {'default': env.db_url('DATABASE_URL')}
 
-ALLOWED_HOSTS = ['139.59.185.182']
+ALLOWED_HOSTS = ['46.101.49.99', 'www.coombewoodltc.com', 'sandbox.coombewoodltc.com']
 SECURE_SSL_REDIRECT = False
 
 TEMPLATES[0]['OPTIONS']['debug'] = DEBUG
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_files/')
 SECRET_KEY = env.str('SECRET_KEY')
 GO_CARDLESS = env.dict('GO_CARDLESS_SANDBOX')
 BEE_FREE_ID = env.str('BEE_FREE_ID')
 BEE_FREE_SECRET = env.str('BEE_FREE_SECRET')
+
+EMAIL_BACKEND = EMAIL_BACKEND = 'anymail.backends.mailgun.MailgunBackend'
 ANYMAIL = env.dict('ANYMAIL')
