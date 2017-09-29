22e045de4d39524840b3e6723857bac4165992f5
ian <is@ktconsultants.co.uk>
2017-09-29 19:41:01 +0100

    Added django-celery-email to requirements - 2

--- mysite/settings/digitalocean_sandbox.py
+++ mysite/settings/digitalocean_sandbox.py
@@ -7,7 +7,7 @@ env_path = os.path.join(BASE_DIR, "mysite", "settings", ".env")
 environ.Env.read_env(env_path)
 
 INSTALLED_APPS += (
-    'djcelery_email'
+    'djcelery_email',
     'debug_toolbar',
 #    'django_nose',
 )
