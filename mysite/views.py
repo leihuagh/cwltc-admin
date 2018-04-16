adff3f4580c282060ed1ba5dfaeabfdc557e3b90
ian <is@ktconsultants.co.uk>
2018-04-16 11:06:40 +0100

    Custom 404 page and state_text properties in Invoice and Payment models

--- mysite/views.py
+++ mysite/views.py
@@ -8,21 +8,6 @@ def index_view(request):
     return redirect('http://www.coombewoodltc.co.uk/')
 
 
-def handler404(request):
-    response = render_to_response('404.html', {},
-                                  context_instance=RequestContext(request))
-    response.status_code = 404
-    return response
-
-
-
-def handler500(request):
-    response = render_to_response('500.html', {},
-                                  context_instance=RequestContext(request))
-    response.status_code = 500
-    return response
-
-
 def test_celery_view(request):
     addxy.delay(10, 20)
     return redirect('home')
