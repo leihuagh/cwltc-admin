2483824f8e39a0ab30d511e8758827cfc29a15f1
ian <is@ktconsultants.co.uk>
2018-06-29 18:26:21 +0100

    Add pos_payments management command to fix rounding errors

--- members/management/commands/pos_payments.py
+++ members/management/commands/pos_payments.py
@@ -1,12 +1,36 @@
 from django.core.management.base import BaseCommand
-
-from pos.services import fix_tea_transactions
+from django.db.models import Sum
+from pos.models import PosPayment, Transaction
 
 
 class Command(BaseCommand):
-    help = 'Change type to Teas'
+    help = 'Check and possibly fix errors in Pos payments'
 
+    def add_arguments(self, parser):
+        parser.add_argument('--fix', help='Update payment from transaction')
 
     def handle(self, *args, **options):
-        fix_tea_transactions()
+        payments = PosPayment.objects.filter(transaction__billed=False,
+                                             transaction__split=False).select_related('transaction')
+        self.stdout.write('Checking non-split transactions')
+        for pay in payments:
+            if pay.amount != pay.transaction.total:
+                self.stdout.write(self.style.ERROR(f'Payment Id: {pay.id} Amount: {pay.amount} '
+                                                   f'Transaction id: {pay.transaction.id} {pay.transaction.total}'))
+                if options['fix']:
+                    pay.amount = pay.transaction.total
+                    pay.save()
+        self.stdout.write('Checking split transactions')
+        splits = Transaction.objects.filter(billed=False, split=True)
+        for split in splits:
+            sum = PosPayment.objects.filter(transaction__id=split.id).aggregate(sum=Sum('amount'))['sum']
+            if sum != split.total:
+                self.stdout.write(self.style.ERROR(f'Payments total: {sum} '
+                                                   f'Transaction id: {split.id} {split.total}'))
+                if options['fix']:
+                    diff = sum - split.total
+                    pay1 = PosPayment.objects.filter(transaction__id=split.id)[0]
+                    pay1.amount = pay1.amount - diff
+                    pay1.save()
+
 
