c4d4e6ff5a442337d4fb3ebc5662765959a0efcc
ian <is@ktconsultants.co.uk>
2018-07-26 21:27:29 +0100

    beat logger

--- mysite/tasks.py
+++ mysite/tasks.py
@@ -14,10 +14,10 @@ app = Celery()
 @app.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
     # Calls test('hello') every 10 seconds.
-    sender.add_periodic_task(10.0, test('hello'), name='add every 10')
+    sender.add_periodic_task(10.0, test.s('hello'), name='add every 10')
 
     # Calls test('world') every 30 seconds
-    sender.add_periodic_task(30.0, test('world'), expires=10)
+    sender.add_periodic_task(30.0, test.s('world'), expires=10)
 
     # Executes every Monday morning at 7:30 a.m.
     sender.add_periodic_task(
